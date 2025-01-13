8fc9a75a82e3fe41eeb6fabcea3eb024840762b7
Mar <mar@strawmelonjuice.com>
2025-01-13 14:27:13 +0100

    Yup.

--- dbscript_mysql_horeca.sql
+++ dbscript_mysql_horeca.sql
@@ -1,7 +1,7 @@
 -- Naar MySQL geconverteerd.
 CREATE DATABASE IF NOT EXISTS horeca;
 USE horeca;
-DROP TABLE IF EXISTS `diner`;
+DROP TABLE IF EXISTS diner;
 CREATE TABLE `diner` (
   `dinner_id` int(11) NOT NULL,
   `description` varchar(250) DEFAULT NULL,
@@ -11,10 +11,13 @@ CREATE TABLE `diner` (
   `image` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `diner` (`dinner_id`, `description`, `start`, `end`, `location`) VALUES
-(1, 'ZomerBrunch', '2021-06-03 10:00:00', '2021-06-03 17:00:00', 'Binnenplaats Vlijmense weg'),
-(2, 'Heerboeren diner', '2021-06-04 13:00:00', '2021-06-04 16:00:00', 'Ons restaurant'),
-(3, 'All you can eat - Frikandellenbroodjes', '2021-06-05 09:00:00', '2021-06-05 17:00:00', 'Studentenkantine');
+INSERT INTO `diner` (`dinner_id`, `description`, `start`, `end`, `location`, `image`) VALUES
+(1, 'ZomerBrunch', '2021-06-03 10:00:00', '2021-06-03 17:00:00', 'Binnenplaats Vlijmense weg', ''),
+(2, 'Heerboeren diner', '2021-06-04 13:00:00', '2021-06-04 16:00:00', 'Ons restaurant', ''),
+(3, 'All you can eat - Frikandellenbroodjes', '2021-06-05 09:00:00', '2021-06-05 17:00:00', 'Studentenkantine', '');
+
+
+UPDATE `diner` SET `image` = NULL WHERE `image` = '';
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -33,7 +36,8 @@ INSERT INTO `user` (`user_name`, `password`, `student_nr`) VALUES
 ('PhilipBoekholt', 'welkom123', '56109212'),
 ('SelamiMeulenbroeks', 'welkom123', '89084537'),
 ('SemiraTrines', 'welkom123', '28868799'),
-('SiebrenBreukers', 'welkom123', '93561432');
+('SiebrenBreukers', 'welkom123', '93561432'),
+('Just_Mar_OK34', 'xxosihH8riEq7c', '505435');
 
 
 ALTER TABLE `diner`
